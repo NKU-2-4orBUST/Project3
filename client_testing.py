7a869d3eaefe5920cda0eea35f0c9c5ea430a752
atrenkamp <85128870+atrenkamp@users.noreply.github.com>
2021-06-19 15:56:19 -0400

    added umount home

---
+++ client_testing.py
@@ -0,0 +1,22 @@
+from os import system as s
+
+# Verifiy Monit monitors and restarts SSH
+s('echo ""; echo "Testing SSH..."; systemctl stop sshd; monit; sleep 5; monit summary | grep SSH; sleep 120; monit summary | grep SSH')
+
+# Verifiy Monit monitors and restarts RSYSLOG
+s('echo ""; echo "Testing RSYSLOG..."; systemctl stop rsyslog; monit; sleep 5; monit summary | grep RSYSLOG; sleep 35; monit summary | grep RSYSLOG')
+
+# Verifiy Monit monitors and restarts SENDMAIL
+s('echo ""; echo "Testing SENDMAIL..."; systemctl stop sendmail; monit; sleep 5; monit summary | grep SENDMAIL; sleep 35; monit summary | grep SENDMAIL')
+
+# Verifiy Monit monitors Memory usages
+s('echo ""; echo "Testing System Memory Monitoring..."; monit; sleep 5; monit summary | grep Group-2-CIT470-NKU-EDU; stress --vm  1 --vm-bytes 3500M --timeout 45s; monit summary | grep Group-2-CIT470-NKU-EDU')
+
+# Verifiy Monit monitors CPU usages
+s('echo ""; echo "Testing System CPU Monitoring..."; monit; sleep 5; monit summary | grep Group-2-CIT470-NKU-EDU; stress --vm-bytes 512M --cpu 100 --timeout 90s; monit summary | grep Group-2-CIT470-NKU-EDU')
+
+# Verifiy Monit monitors Disk Space usages
+s('echo ""; echo "Testing Monit Disk Monitoring..."; monit summary | grep -e HOME -e ROOT -e VAR; echo ""; echo "Filling /HOME..."; dd if=/dev/zero of=/home/zero bs=51200 count=1000000; echo ""; echo "Filling /VAR..."; dd if=/dev/zero of=/var/zero bs=51200 count=1000000; echo ""; echo "Filling /..."; dd if=/dev/zero of=/zero bs=51200 count=1000000; echo ""; echo "Waiting for Monit Cycles"; sleep 90; monit summary | grep -e HOME -e ROOT -e VAR;  echo ""; echo "Cleaning up disk test..."; rm -f /home/zero; rm -f /var/zero; rm -f /zero')
+
+# Verifiy Monit monitors /HOME Mounting
+s('echo ""; echo "Testing Monit /HOME mounting..."; monit summary | grep -e HOME; echo ""; echo "Unmounting /HOME..."; umount /home; monit; monit summary | grep -e HOME; echo "Waiting for Monit Cycles"; sleep 60; monit summary | grep -e HOME')
