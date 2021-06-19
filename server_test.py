06d3965d569b6fcf64272be41da7cbea31d1ce99
atrenkamp <85128870+atrenkamp@users.noreply.github.com>
2021-06-19 12:43:45 -0400

    added sleep 5 to cpu test

--- server_test.py
+++ server_test.py
@@ -16,7 +16,7 @@ s('echo "Testing RSYSLOG..."; systemctl stop rsyslog; monit; sleep 5; monit summ
 s('echo "Testing System CPU Monitoring..."; monit; sleep 5; monit summary | grep Group-2-CIT470-NKU-EDU; stress --vm  1 --vm-bytes 3500M --timeout 45s; monit summary | grep Group-2-CIT470-NKU-EDU')
 
 # Verifiy Monit monitors CPU usages
-s('echo "Testing System CPU Monitoring..."; monit; monit summary | grep Group-2-CIT470-NKU-EDU; stress --vm-bytes 256M --cpu 100 --timeout 40s; monit summary | grep Group-2-CIT470-NKU-EDU')
+s('echo "Testing System CPU Monitoring..."; monit; sleep 5; monit summary | grep Group-2-CIT470-NKU-EDU; stress --vm-bytes 256M --cpu 100 --timeout 40s; monit summary | grep Group-2-CIT470-NKU-EDU')
 
 # Verifiy Monit monitors Disk Space usages
 s('echo "Testing Monit Disk Monitoring..."; monit summary | grep -e HOME -e ROOT -e VAR; echo "Filling /HOME..."; dd if=/dev/zero of=/home/zero bs=51200 count=1000000; echo "Filling /VAR..."; dd if=/dev/zero of=/var/zero bs=51200 count=1000000; echo "Filling /..."; dd if=/dev/zero of=/zero bs=51200 count=1000000; echo "Waiting for Monit Cycles"; sleep 90; monit summary | grep -e HOME -e ROOT -e VAR;  echo "Cleaning up disk test..."; rm -f /home/zero; rm -f /var/zero; rm -f /zero')
