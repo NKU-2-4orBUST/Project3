7ce04c603cbf1003425789fd0de159c537ac6e2a
atrenkamp <85128870+atrenkamp@users.noreply.github.com>
2021-06-19 13:58:08 -0400

    added sendmail test

--- server_test.py
+++ server_test.py
@@ -12,6 +12,9 @@ s('echo ""; echo "Testing LDAP..."; systemctl stop slapd; monit; sleep 5; monit 
 # Verifiy Monit monitors and restarts RSYSLOG
 s('echo ""; echo "Testing RSYSLOG..."; systemctl stop rsyslog; monit; sleep 5; monit summary | grep RSYSLOG; sleep 35; monit summary | grep RSYSLOG')
 
+# Verifiy Monit monitors and restarts RSYSLOG
+s('echo ""; echo "Testing SENDMAIL..."; systemctl stop sendmail; monit; sleep 5; monit summary | grep SENDMAIL; sleep 35; monit summary | grep SENDMAIL')
+
 # Verifiy Monit monitors Memory usages
 s('echo ""; echo "Testing System Memory Monitoring..."; monit; sleep 5; monit summary | grep Group-2-CIT470-NKU-EDU; stress --vm  1 --vm-bytes 3500M --timeout 45s; monit summary | grep Group-2-CIT470-NKU-EDU')
 
