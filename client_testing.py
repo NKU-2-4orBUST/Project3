87820886a40676649af7cfa0533fb0a2a967099f
atrenkamp <85128870+atrenkamp@users.noreply.github.com>
2021-06-19 22:52:02 -0400

    Update client_testing.py

--- client_testing.py
+++ client_testing.py
@@ -4,22 +4,22 @@ from os import system as s
 s('yum -y install stress')
 
 # Verifiy Monit monitors and restarts SSH
-s('echo ""; echo "Testing SSH..."; systemctl stop sshd; monit summary | grep SSH; sleep 35; monit summary | grep SSH')
+s('echo ""; echo "Testing SSH..."; systemctl stop sshd; monit summary | grep SSH; monit; sleep 30; monit summary | grep SSH')
 
 # Verifiy Monit monitors and restarts RSYSLOG
-s('echo ""; echo "Testing RSYSLOG..."; systemctl stop rsyslog; monit; sleep 5; monit summary | grep RSYSLOG; sleep 35; monit summary | grep RSYSLOG')
+s('echo ""; echo "Testing RSYSLOG..."; systemctl stop rsyslog; monit; sleep 1; monit summary | grep RSYSLOG; sleep 35; monit summary | grep RSYSLOG')
 
 # Verifiy Monit monitors and restarts SENDMAIL
-s('echo ""; echo "Testing SENDMAIL..."; systemctl stop sendmail; monit; sleep 5; monit summary | grep SENDMAIL; echo "Waiting 2 minutes for SENDMAIL services to restart"; sleep 120; monit summary | grep SENDMAIL')
+s('echo ""; echo "Testing SENDMAIL..."; systemctl stop sendmail; monit; sleep 1; monit summary | grep SENDMAIL; echo "Waiting 2 minutes for SENDMAIL services to restart"; sleep 120; monit summary | grep SENDMAIL')
 
 # Verifiy Monit monitors and restarts LDAP
-s('echo ""; echo "Testing LDAP..."; systemctl stop nslcd; monit; sleep 5; monit summary | grep LDAP; sleep 35; monit summary | grep LDAP')
+s('echo ""; echo "Testing LDAP..."; systemctl stop nslcd; monit; sleep 1; monit summary | grep LDAP; sleep 35; monit summary | grep LDAP')
 
 # Verifiy Monit monitors Memory usages
-s('echo ""; echo "Testing System Memory Monitoring..."; monit; sleep 5; monit summary | grep System; stress --vm  1 --vm-bytes 3500M --timeout 45s; monit summary | grep System')
+s('echo ""; echo "Testing System Memory Monitoring..."; monit; sleep 1; monit summary | grep System; stress --vm  1 --vm-bytes 3500M --timeout 45s; monit summary | grep System')
 
 # Verifiy Monit monitors CPU usages
-s('echo ""; echo "Testing System CPU Monitoring..."; monit; sleep 5; monit summary | grep System; stress --vm-bytes 512M --cpu 100 --timeout 90s; monit summary | grep System')
+s('echo ""; echo "Testing System CPU Monitoring..."; monit; sleep 1; monit summary | grep System; stress --vm-bytes 512M --cpu 100 --timeout 90s; monit summary | grep System')
 
 # Verifiy Monit monitors Disk Space usages
 s('echo ""; echo "Testing Monit Disk Monitoring..."; monit summary | grep -e HOME -e ROOT -e VAR; echo ""; echo "Filling /HOME..."; dd if=/dev/zero of=/home/zero bs=51200 count=1000000; echo ""; echo "Filling /VAR..."; dd if=/dev/zero of=/var/zero bs=51200 count=1000000; echo ""; echo "Filling /..."; dd if=/dev/zero of=/zero bs=51200 count=1000000; echo ""; echo "Waiting for Monit Cycles"; sleep 90; monit summary | grep -e HOME -e ROOT -e VAR;  echo ""; echo "Cleaning up disk test..."; rm -f /home/zero; rm -f /var/zero; rm -f /zero')
